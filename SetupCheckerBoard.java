abf059009d0d51f8f3c737511b140c9f482a5ba1
kevin115533 <kevin115533@yahoo.com>
2018-10-29 21:57:15 -0500

    pieces now laid on board

--- SetupCheckerBoard.java
+++ SetupCheckerBoard.java
@@ -8,7 +8,7 @@ public class SetupCheckerBoard{
 		Scanner input1 = new Scanner(System.in);
 
 			Board newGame = new Board();
-			CheckersPiece[] blk = new CheckersPiece[count];	
+			CheckersPiece[] black = new CheckersPiece[count];	
 			CheckersPiece[] red = new CheckersPiece[count];
 			
 			for(int i = 0;i<count;i++){
@@ -16,31 +16,11 @@ public class SetupCheckerBoard{
 			}
 		
 			for(int i = 0;i<count;i++){
-				blk[i] = new CheckersPiece(Color.B,i);
+				black[i] = new CheckersPiece(Color.B,i);
 			}
-			
-		/*for(int i = 0;i<count;i++){
-				for(int j = 0;j<count;j++){
-				if(j == 0 | j == 2){
-					if(i == 1 | i == 3 | i == 5 | i == 7){
-					newGame.setSpot(i,j,red[i]);}
-						}
-				if(j == 1){
-					if(i == 0 | i == 2 | i == 4 | i == 6){
-					newGame.setSpot(i,j,red[i]);}	
-						}
-					}
- 				}*/
 		
-		for(int i = 0;i<count;i++){
-			for(int j=0;j<3;j++){
-				if(j ==0){
-				newGame.setSpot(k,j,red[i]);
-					}
-			}
- 				}
-		
-		/*newGame.setSpot(1,0,red[0]);
+		//manually setting peices on board. Dont think theres a loop that could condense it down.
+		newGame.setSpot(1,0,red[0]);
 		newGame.setSpot(3,0,red[1]);
 		newGame.setSpot(5,0,red[2]);
 		newGame.setSpot(7,0,red[3]);
@@ -51,7 +31,20 @@ public class SetupCheckerBoard{
 		newGame.setSpot(1,2,red[8]);
 		newGame.setSpot(3,2,red[9]);
 		newGame.setSpot(5,2,red[10]);
-		newGame.setSpot(7,2,red[11]);*/
+		newGame.setSpot(7,2,red[11]);
+		
+		newGame.setSpot(0,5,black[0]);
+		newGame.setSpot(2,5,black[1]);
+		newGame.setSpot(4,5,black[2]);
+		newGame.setSpot(6,5,black[3]);
+		newGame.setSpot(1,6,black[4]);
+		newGame.setSpot(3,6,black[5]);
+		newGame.setSpot(5,6,black[6]);
+		newGame.setSpot(7,6,black[7]);
+		newGame.setSpot(0,7,black[8]);
+		newGame.setSpot(2,7,black[9]);
+		newGame.setSpot(4,7,black[10]);
+		newGame.setSpot(5,7,black[11]);
 		
 		newGame.printBoard();
 	}
